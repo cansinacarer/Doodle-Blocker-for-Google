adbed15e03a3b2f4d63fc54242e8e4258f978137
Cansin Acarer <cansinacarer@users.noreply.github.com>
2022-08-15 10:11:09 -0400

    updated to satisfy manifest v3 requirements

--- action.js
+++ action.js
@@ -1,15 +1,5 @@
-$( document ).ready(function() {
-	$("#lga").attr("id", "newlga");
-	$("#hplogo").attr("id", "newhplogo");
-	$("#newhplogo").html('<img id="imgLogo">');
-	$("#newhplogo").removeAttr("title");
-	$("#imgLogo").attr("src", chrome.extension.getURL("google-logo.svg"));
-
-	$("#logocont").html('<img id="imgLogoSmall">');
-	$("#newhplogo").removeAttr("title");
-	$("#newhplogo").removeAttr("tabindex");
-	$("#imgLogoSmall").attr("src", chrome.extension.getURL("google-logo.svg"));
-
-
-
+$(document).ready(function () {
+	$(".o3j99.LLD4me.yr19Zb.LS8OJ").html(
+		'<style data-iml="1660571331865">.LS8OJ {display: flex;flex-direction: column;align-items: center}.k1zIA {height: 100%;margin-top: auto}</style><div class="k1zIA rSk4se" doodle-blocker><style>.rSk4se {max-height: 92px;position: relative}.lnXdpd {max-height: 100%;max-width: 100%;object-fit: contain;object-position: center bottom;width: auto}</style><img class="lnXdpd" alt="Google" height="92"src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"srcset="/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png 1x, /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png 2x"width="272" data-atf="1" data-frt="0"></div>'
+	);
 });
