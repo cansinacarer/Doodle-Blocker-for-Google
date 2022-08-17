609c1975d0acc81e1851957b097ca667948345e8
Cansin Acarer <cansinacarer@users.noreply.github.com>
2022-08-17 16:53:35 -0400

    fixed logo replacement in the results pages

--- action.js
+++ action.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
 	$(".o3j99.LLD4me.yr19Zb.LS8OJ").html(
 		'<style data-iml="1660571331865">.LS8OJ {display: flex;flex-direction: column;align-items: center}.k1zIA {height: 100%;margin-top: auto}</style><div class="k1zIA rSk4se" doodle-blocker><style>.rSk4se {max-height: 92px;position: relative}.lnXdpd {max-height: 100%;max-width: 100%;object-fit: contain;object-position: center bottom;width: auto}</style><img class="lnXdpd" alt="Google" height="92"src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"srcset="/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png 1x, /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png 2x"width="272" data-atf="1" data-frt="0"></div>'
 	);
+	$(".logo").html(
+		'<a href="https://www.google.ca/" title="Go to Google Home" id="logo" data-hveid="8"><img src="/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" alt="Google" height="30" width="92" data-atf="1" data-frt="0"></a>'
+	);
 });
